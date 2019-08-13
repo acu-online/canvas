4481054649a3ed9e87d05ca06b8c7d54b82f5447
John Kaczmarek <johnkacz@gmail.com>
2019-08-13 13:11:04 -0500

    Add User resource

---
+++ lib/canvas/resources/user.ex
@@ -0,0 +1,31 @@
+defmodule Canvas.Resources.User do
+  @moduledoc """
+  Represents an User.
+
+  See:
+  - https://canvas.instructure.com/doc/api/users
+  - https://canvas.instructure.com/doc/api/users#User
+  """
+
+  @type t :: %__MODULE__{
+          id: integer,
+          name: String.t(),
+          sortable_name: String.t(),
+          short_name: String.t(),
+          sis_user_id: String.t(),
+          sis_import_id: integer,
+          integration_id: String.t(),
+          login_id: String.t(),
+          avatar_url: String.t(),
+          enrollments: any,
+          email: String.t(),
+          locale: String.t(),
+          last_login: String.t(),
+          time_zone: String.t(),
+          bio: String.t()
+        }
+
+  defstruct ~w(id name sortable_name short_name sis_user_id sis_import_id
+               integration_id login_id avatar_url enrollments email locale
+               last_login time_zone bio)a
+end
---
+++ lib/canvas/resources/users.ex
@@ -0,0 +1,123 @@
+defmodule Canvas.Resources.Users do
+  @moduledoc """
+  Provides functions to interact with the
+  [course endpoints](https://canvas.instructure.com/doc/api/users).
+  """
+
+  alias Canvas.{Client, Listing, Response}
+  alias Canvas.Resources.User
+
+  @doc """
+  A paginated list of of users associated with this account.
+
+  See:
+  - https://canvas.instructure.com/doc/api/users#method.users.index
+
+  ## Examples:
+
+      client = %Canvas.Client{access_token: "a1b2c3d4", base_url: "https://instructure.test"}
+      {:ok, response} = Canvas.Resources.Users.list_users_in_account(client, account_id = 1)
+
+  """
+  @spec list_users_in_account(Client.t(), String.t() | integer, Keyword.t()) ::
+          {:ok | :error, Response.t()}
+  def list_users_in_account(client, account_id, options \\ []) do
+    url = Client.versioned("/accounts/#{account_id}/users")
+
+    Listing.get(client, url, options)
+    |> Response.parse([%User{}])
+  end
+
+  @doc """
+  A paginated list of of users associated with this account.
+
+  See:
+  - https://canvas.instructure.com/doc/api/users#method.users.index
+
+  ## Examples:
+
+      client = %Canvas.Client{access_token: "a1b2c3d4", base_url: "https://instructure.test"}
+      {:ok, response} = Canvas.Resources.Users.all_users_in_account(client, account_id = 1)
+
+  """
+  @spec all_users_in_account(Client.t(), String.t() | integer, Keyword.t()) ::
+          {:ok | :error, Response.t()}
+  def all_users_in_account(client, account_id, options \\ []) do
+    Listing.get_all(__MODULE__, :list_users_in_account, [client, account_id, options])
+  end
+
+  def list_the_activity_stream() do
+  end
+
+  def activity_stream_summary() do
+  end
+
+  def list_the_todo_items() do
+  end
+
+  def list_counts_for_todo_items() do
+  end
+
+  def list_upcoming_assignments_calendar_events() do
+  end
+
+  def list_missing_submissions() do
+  end
+
+  def hide_a_stream_item() do
+  end
+
+  def hide_all_stream_items() do
+  end
+
+  def upload_a_file() do
+  end
+
+  def show_user_details() do
+  end
+
+  def create_a_user() do
+  end
+
+  def self_register_a_user() do
+  end
+
+  def update_user_settings() do
+  end
+
+  def get_custom_colors() do
+  end
+
+  def get_custom_color() do
+  end
+
+  def update_custom_color() do
+  end
+
+  def get_dashboard_positions() do
+  end
+
+  def update_dashboard_positions() do
+  end
+
+  def edit_a_user() do
+  end
+
+  def merge_user_into_another_user() do
+  end
+
+  def split_merged_users_into_separate_users() do
+  end
+
+  def get_a_pandata_events_jwt_token_and_its_expiration_date() do
+  end
+
+  def get_a_users_most_recently_graded_submissions() do
+  end
+
+  def get_user_profilelist_avatar_options() do
+  end
+
+  def list_user_page_views() do
+  end
+end
