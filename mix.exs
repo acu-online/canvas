c96c7a6a681626472346c57958017dee92f99bcc
John Kaczmarek <johnkacz@gmail.com>
2020-04-21 01:34:23 -0500

    Update elixir version to 1.10 and update deps.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Canvas.MixProject do
     [
       app: :canvas,
       version: "0.0.1",
-      elixir: "~> 1.8",
+      elixir: "~> 1.10",
       description: "Elixir client for the Canvas LMS API",
       start_permanent: Mix.env() == :prod,
       package: package(),
